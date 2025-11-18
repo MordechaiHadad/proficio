8af82c1c569b154684c849da3b63a3340f298348
MordechaiHadad <mordechai.hadad01@gmail.com>
2025-11-18 14:50:28 +0200

    Enhance layout and navigation structure in +layout.svelte for bigger screens

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -45,18 +45,48 @@
 </script>
 
 <div
-    class="relative h-svh w-full overflow-y-auto flex flex-col items-center gap-4 pt-4 px-4 pb-24 bg-gray-100 dark:bg-gray-900">
+    class="relative h-svh w-full overflow-y-auto flex flex-col items-center gap-4 pt-4 md:pt-16 md:gap-8 px-4 pb-24 bg-gray-100 dark:bg-gray-900">
     <div class="flex gap-2 flex-col items-center">
         <h1 class="text-2xl font-semibold dark:text-neutral-100">Proficio</h1>
         <p class="text-sm text-gray-600 dark:text-gray-400">
             Track your progress, break bad habits
         </p>
     </div>
+    <nav
+        class="hidden lg:grid grid-cols-3 gap-1 bg-neutral-200/30 dark:bg-gray-800 rounded-lg p-1">
+        <a
+            href="/"
+            class="flex place-content-center place-items-center py-2 px-4 gap-1 w-full h-full rounded-md text-gray-600 dark:text-gray-400 hover:text-sky-600 dark:hover:text-blue-500 text-sm {currentRoute ===
+            '/'
+                ? 'dark:text-blue-500! text-sky-600 bg-white dark:bg-gray-900'
+                : ''}">
+            <LayoutDashboard class="size-4" />
+            <span>Dashboard</span>
+        </a>
+        <a
+            href="/add"
+            class="flex place-content-center place-items-center py-2 px-4 gap-1 w-full h-full rounded-md text-gray-600 dark:text-gray-400 hover:text-sky-600 dark:hover:text-blue-500 text-sm {currentRoute ===
+            '/add'
+                ? 'dark:text-blue-500! text-sky-600 bg-white dark:bg-gray-900'
+                : ''}">
+            <Plus class="size-4" />
+            <span>Add Habit</span>
+        </a>
+        <a
+            href="/settings"
+            class="flex place-content-center place-items-center gap-1 py-2 px-4 w-full h-full rounded-md text-gray-600 dark:text-gray-400 hover:text-sky-600 dark:hover:text-blue-500 text-sm {currentRoute ===
+            '/settings'
+                ? 'dark:text-blue-500! text-sky-600 bg-white dark:bg-gray-900'
+                : ''}">
+            <Settings class="size-4" />
+            <span>Settings</span>
+        </a>
+    </nav>
     <div class="w-full max-w-5xl">
         {@render children()}
     </div>
     <nav
-        class="grid grid-cols-3 fixed bottom-0 left-0 right-0 justify-between z-10 w-full h-20 p-2 bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700">
+        class="grid grid-cols-3 fixed bottom-0 left-0 right-0 justify-between z-10 w-full h-20 p-2 bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 lg:hidden">
         <a
             href="/"
             class="flex flex-col place-content-center place-items-center gap-1 w-full h-full rounded-md text-gray-600 dark:text-gray-400 hover:text-sky-600 dark:hover:text-blue-500 text-sm {currentRoute ===
