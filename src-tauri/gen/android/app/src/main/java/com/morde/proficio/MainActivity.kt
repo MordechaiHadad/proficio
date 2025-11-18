bfc589982b7fb6bbc7a80e3ee780a60ebb6643a5
MordechaiHadad <mordechai.hadad01@gmail.com>
2025-11-18 17:35:59 +0200

    Enable system window fitting in MainActivity for improved layout handling

--- src-tauri/gen/android/app/src/main/java/com/morde/proficio/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/morde/proficio/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.morde.proficio
 
 import android.os.Bundle
+import androidx.core.view.WindowCompat 
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    WindowCompat.setDecorFitsSystemWindows(window, true)
   }
 }
