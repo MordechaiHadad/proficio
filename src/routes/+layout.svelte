ea5952289c761269da80ee199e5fab3df2324bff
MordechaiHadad <mordechai.hadad01@gmail.com>
2025-11-19 16:02:56 +0200

    Refactor layout class for improved readability

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -46,7 +46,7 @@
 </script>
 
 <div
-    class="relative min-h-dvh w-full overflow-y-auto flex flex-col items-center gap-4 {currentPlatform === 'android' || currentPlatform === 'ios' ? 'pt-16 pb-31' : 'pt-4 pb-24'} md:pt-16 md:gap-8 px-4 bg-gray-100 dark:bg-gray-900">
+    class="relative min-h-dvh w-full overflow-y-auto flex flex-col items-center gap-4 {currentPlatform === 'android' || currentPlatform === 'ios' ? 'pt-16 pb-31' : 'pt-4'} md:pt-16 md:gap-8 px-4 bg-gray-100 dark:bg-gray-900">
     <div class="flex gap-2 flex-col items-center">
         <h1 class="text-2xl font-semibold dark:text-neutral-100">Proficio</h1>
         <p class="text-sm text-gray-600 dark:text-gray-400">
